c8bdc51aa31630c34e281764040ca747da0036e2
Manish P <invincible.manish1417@gmail.com>
2022-03-03 20:51:29 +0530

    added two routes

---
+++ models/sensor.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const sensorSchema = new mongoose.Schema({
+    // _id: {
+    //     type: Number,
+    //     unique: true
+    // },
+    sensorId: {
+        type: String,
+        required: true
+    },
+    temperature: {
+        type: String,
+        required: true
+    },
+    humidity: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+})
+
+const Sensor = mongoose.model("Sensor", sensorSchema)
+
+module.exports = Sensor
+
---
+++ server.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const express = require("express");
+require('dotenv').config();
+
+const Sensor = require("./models/sensor");
+//const router = require('express').Router();
+const app = express();
+
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
+
+const config = mongoose.connect(
+    MONGO_URI,
+    {},
+    ()=> {
+    console.log("DB connected")
+})
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    try{
+        console.log("Home Page")
+        const data = await Sensor.find()
+        res.send(data)
+    } catch (err) {
+        console.log(err)
+    } 
+});
+
+app.post("/", async (req, res) => {
+    try {
+        console.log(req.body);
+        const data = { ...req.body }
+        const newSensor = new Sensor(data)
+        await newSensor.save()
+        res.send("Data appended successfully")
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+app.listen(PORT, () => {
+    config;
+    console.log(`Server Running at Port:${PORT}`)
+})
